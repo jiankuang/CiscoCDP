e68fd52aaa6e8444b7d3eb1847398aa19843e4bb
jkuang <jian.j.kuang@gmail.com>
2017-03-24 16:19:09 -0700

    change </script>

--- cdp-webapp/parkingAPI.js
+++ cdp-webapp/parkingAPI.js
@@ -1,3 +1,5 @@
+alert("test");
+
 // ------------------------------ get user info ---------------------------------------------------------
 // jQuery.ajax({
 // 	url: "http://10.10.20.6/apigw/devnetlabapi/cdp/v1/accounts/username?loginName=devoperator9%40cdp.com", 
@@ -15,45 +17,52 @@
 // 	}
 // });
 
-// ------------------------------- get all the lights ----------------------------------------------------
-jQuery.ajax({
-	url: "http://10.10.20.6/apigw/devnetlabapi/cdp/v1/devices/lighting?UserKey=500109&SensorCustomerKey=500050&AppKey=CDP-App", 
-	type: "POST",
-	data: JSON.stringify({"Query":
-	    {"Find":
-	        {"Light":
-	            {"sid":
-	                {"ne":""}
-	            }
-	        }
-	    }
-	}),
-	beforeSend: function(xhr) {
-		xhr.setRequestHeader('WSO2-Authorization', 'oAuth Bearer 32b37facf21b4deadc93a261e2dc4f2');
-		xhr.setRequestHeader('Authorization', 'Bearer 9sY1mKjZgJDa0yw3LHBdaJetU68y');
-		xhr.setRequestHeader('Accept', 'application/json');
-		xhr.setRequestHeader('Content-Type', 'application/json');
-	},
-	success: function(data){
-		var result = [];
+// ------------------------------- get all the Parking Spots ----------------------------------------------------
 
-		lights = data['Find']['Result'];
+function getAllParkingSpots() {
 
-		console.log(lights.length);
+	spots = 
 
-		for(var i=0; i < lights.length; i++) {
-			var light = lights[i]['Light']
-			var sid = light['sid'];
-			var intensityLevel = light['state']['intensityLevel'];
+	jQuery.ajax({
+		url: "http://10.10.20.6/apigw/devnetlabapi/cdp/v1/devices/parking?UserKey=500109&SensorCustomerKey=500050&AppKey=CDP-App", 
+		type: "POST",
+		data: JSON.stringify({"Query":
+		    {"Find":
+		        {"ParkingSpot":
+		            {"sid":
+		                {"ne":""}
+		            }
+		        }
+		    }
+		}),
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader('WSO2-Authorization', 'oAuth Bearer 32b37facf21b4deadc93a261e2dc4f2');
+			xhr.setRequestHeader('Authorization', 'Bearer 9sY1mKjZgJDa0yw3LHBdaJetU68y');
+			xhr.setRequestHeader('Accept', 'application/json');
+			xhr.setRequestHeader('Content-Type', 'application/json');
+		},
+		success: function(data){
+			var result = [];
 
-			result.push({"sid": sid, "intensityLevel": intensityLevel});
+			spots = data['Find']['Result'];
+
+			console.log(spots.length);
+
+			for(var i=0; i < spots.length; i++) {
+				var spot = spots[i]['Light']
+				var sid = spot['sid'];
+				var occupied = spot['state']['occupied'];
+
+				result.push({"sid": sid, "occupied": occupied});
+			}
+
+			$('#lights').html(JSON.stringify(result));
+			//console.log();
+		}, 
+		error: function(error) {
+			alert("error happend!");
+			alert(error);
 		}
+	});
 
-		$('#lights').html(JSON.stringify(result));
-		//console.log();
-	}, 
-	error: function(error) {
-		alert("error happend!");
-		alert(error);
-	}
-});
+}
